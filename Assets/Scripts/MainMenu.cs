5178d69ef4e60ea6819d51aa953f1ad378ec944f
KrakenTim <KrakenTim@users.noreply.github.com>
2021-04-07 13:01:57 +0200

    Main Menu
    [+] added main menu, version 0.1

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartButton()
+    {
+        SceneManager.LoadScene("");
+    }
+
+    public void QuitButton() 
+    {
+        Application.Quit();
+        Debug.Break();
+    }
+
+   
+}
