6c033abcefc3e1e9b56ec0721f49efce396797a2
KrakenTim <KrakenTim@freenet.de>
2021-05-05 14:43:11 +0200

    Hero fix

--- Assets/Scripts/Tims Scripts/You spin me right round (rotation scripts und scriptversuche)/PlayerRotation.cs
+++ Assets/Scripts/Tims Scripts/You spin me right round (rotation scripts und scriptversuche)/PlayerRotation.cs
@@ -6,10 +6,8 @@ public class PlayerRotation : MonoBehaviour
 {
     void Update()
     {
-    Vector3 position = Camera.current.WorldToScreenPoint(transform.position);
-    Vector3 direction = Input.mousePosition - position;
+    Vector3 direction = Input.mousePosition - Camera.current.WorldToScreenPoint(transform.position); ;
     float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
     transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-        
     }
 }
