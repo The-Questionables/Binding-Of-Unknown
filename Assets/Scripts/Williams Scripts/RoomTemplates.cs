10ed1c05c6f4e7b4522f4e60bbd79e5475e6a454
WilliamKieburg <battle-franky@hotmail.de>
2021-05-06 11:56:59 +0200

    Adding Enemy Spawner

--- Assets/Scripts/Williams Scripts/RoomTemplates.cs
+++ Assets/Scripts/Williams Scripts/RoomTemplates.cs
@@ -14,7 +14,18 @@ public class RoomTemplates : MonoBehaviour
 
     public List<GameObject> rooms; // Liste an Räumen
 
-    public float waitTime;
+
+    private int random; // speichert zufälligen Wert aus dem Array
+    public GameObject rangeEnemys; // Liste an FernkampfGegnern
+    public int rangeEnemyAmount;
+    private bool spawnedRangeEnemys;
+    private int rangeEnemyAmountCounter = 0;
+
+    public GameObject hellebardeEnemys; // Liste an NahkampfGegnern
+    public int hellebardeEnemysAmount;
+    private bool spawnedHellebardeEnemys;
+
+    public float waitTime = 5f;
     private bool spawnedBoss;
     public GameObject boss; // boss oder Treppe
 
@@ -22,7 +33,43 @@ public class RoomTemplates : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(waitTime <= 0 && spawnedBoss == false)
+        SpawnBossOderTreppe();
+        SpawnRangeEnemys();
+    }
+
+    public void SpawnRangeEnemys()
+    {
+        if(waitTime <= 0 && spawnedRangeEnemys == false)
+        {
+                //  random = Random.Range(0, templates.rightRooms.Length); // Sucht zufälligen Längenwert des BottomRoom Arrays aus
+                //  Instantiate(templates.rightRooms[random], transform.position, Quaternion.identity); // Clonen eines Objektes und erstellen
+
+                // wählt einen zufälligen RaumZahlenWert zum Spawnen aus
+                // für jeden Gegner soll eine zufällige Position ausgesucht werden und gespawnt werden
+           
+                // Spawn der Gegner
+                for (int i = 0; i < rangeEnemyAmount; i++)
+                {
+                    random = Random.Range(0, rooms.Count);
+                    Instantiate(rangeEnemys, rooms[random].transform.position, Quaternion.identity); // Spawnt Enemy am zufällig ausgewählten Raum
+                    rangeEnemyAmountCounter++;
+
+                }
+
+            if (rangeEnemyAmountCounter == rangeEnemyAmount)
+            {
+                spawnedRangeEnemys = true;
+            }
+        }
+        else
+        {
+            waitTime -= Time.deltaTime;
+        }
+    }
+
+    public void SpawnBossOderTreppe()
+    {
+        if (waitTime <= 0 && spawnedBoss == false)
         {
             /*
             for (int i = 0; i < rooms.Count; i++)
