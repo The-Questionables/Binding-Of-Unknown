ae05fe61c68cd30e665859974da9effc7ed6f4cc
KrakenTim <KrakenTim@freenet.de>
2021-05-17 00:15:46 +0200

    Code Remake

--- Assets/Scripts/Tims Scripts/GameManager.cs
+++ Assets/Scripts/Tims Scripts/GameManager.cs
@@ -11,39 +11,40 @@ public class GameManager : MonoBehaviour {
     public Text coinsText;
 
     [Header("Healthpotions:")]
-    public int healpotions;
-    public int maxHealthpotionsSlots;
-    public Text healpotionsText;
-    public Text MaxHealthpotionsSlotsText;
     public bool isHealpotionCollectable;
-
+    public Text healthpotionsText;
+    public KeyCode UseHealthpotion = KeyCode.Q;
+    public int maxHealthpotions;
+    public int healthpotions;
+    public int healthRecover = 10;
 
     [Header("Player Health:")]
-    public int currentHealth;
-    public int maxHealth;
-    public Text currentHealthText;
-    public Text maxHealthText;
+    public int hp;
+    public int maxHp;
+    public Text hpText;
 
-    // Cached References
-    Hero hero;
-
-
-
-    public void Start()
+    public void Update()
     {
-        hero = FindObjectOfType<Hero>();      
-    }
+        healthpotionsText.text = healthpotions.ToString() + "/" + maxHealthpotions.ToString();
+        hpText.text = hp.ToString() + "/" + maxHp.ToString();
 
-    public void Update() 
-    {
-        if (healpotions < maxHealthpotionsSlots) // begrenzen der einsammelbaren Heiltränke
-        { 
-           isHealpotionCollectable = true;
+        if (healthpotions < maxHealthpotions) // begrenzen der einsammelbaren Heiltränke
+        {
+            isHealpotionCollectable = true;
         }
-        else if (healpotions == maxHealthpotionsSlots) 
+        else if (healthpotions == maxHealthpotions)
         {
             isHealpotionCollectable = false;
         }
+
+        if (Input.GetKeyDown(UseHealthpotion) && healthpotions > 0 && hp <= maxHp)
+        {
+            healthpotions--;
+            hp += healthRecover;
+        }
+
+
+
     }
 
     public void AddCoin(int getCoins)
@@ -56,83 +57,187 @@ public class GameManager : MonoBehaviour {
         coinsText.text = "x" + coins.ToString();
     }
 
-    //********************************** Healpotions
-    public void AddHealpotions(int getHealpotions)
-    {
-        healpotions += getHealpotions;
-        UpdateHealpotionsText(healpotions);
-    }
-    public void UpdateHealpotionsText(int healpotions)
-    {
-        healpotionsText.text = "x" + healpotions.ToString();
-    }
-   
-    public void UseHealpotions()
-    {
-        healpotions--; // benutzt - Wert um Healpotion zu nutzen
-        UpdateHealpotionsText(healpotions);
-    }
-
-    //********************************** ShowMaxHealpotionsText
-    public void GetMaxHealthpotionSlotsText(int getMaxHealpotions) // noch einbauen
-    {
-        maxHealthpotionsSlots += getMaxHealpotions;
-        UpdateMaxHealthText(maxHealthpotionsSlots);
-    }
-    public void UpdateMaxHealpotionsSlots(int maxHealthpotions) // noch einbauen
-    {
-        MaxHealthpotionsSlotsText.text = "/" + maxHealthpotions.ToString();
-    }
-
-    //********************************** ShowCurrentHealthText
-    public void GetCurrentHealthText(int getCurrentHealth) // noch einbauen
-    {
-        currentHealth += getCurrentHealth;
-        UpdateCurrentHealthText(currentHealth);
-    }
-    public void UpdateCurrentHealthText(int currentHealth)
-    {
-        currentHealthText.text = currentHealth.ToString();
-    }
-    public void DecreaseCurrentHealthText(int getDamage) // noch einbauen
-    {
-        currentHealth -= getDamage; // benutzt - Wert um Healpotion zu nutzen
-        UpdateHealpotionsText(currentHealth);
-    }
-
-    //********************************** ShowMaxHealthText
-    public void GetMaxHealthText(int getMaxHealth) // noch einbauen
-    {
-        maxHealth += getMaxHealth;
-        UpdateMaxHealthText(maxHealth);
-    }
-    public void UpdateMaxHealthText(int maxHealth) // noch einbauen
-    {
-        maxHealthText.text = "/" + maxHealth.ToString();
-        Debug.Log("UpdateMaxHealthText wurde ausgeführt");
-    }
-    /*
-    public void DecreaseMaxHealthText(int getMaxHealth)
-    {
-        maxHealth -= getMaxHealth; // benutzt - Wert um Healpotion zu nutzen
-        UpdateMaxHealthText(getMaxHealth);
-    }
-    */
 }
 
 
-/*
-    public void ShowStageText(int amount)
-    {
-        // stageText.gameObject.SetActive(true);
-        stageText.text = "Stage " + amount;
 
-        // Invoke("DeactivateStagetext", 3f);
-    }
 
-    //  void DeactivateStagetext()
-    // {
-    //     stageText.gameObject.SetActive(false);
-    //      CancelInvoke("DeactivateStagetext");
-    //  }
-*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine.SceneManagement;
+//using UnityEngine;
+//using UnityEngine.UI;
+
+//public class GameManager : MonoBehaviour {
+
+//    [Header("Coins:")]
+//    public int coins;
+//    public Text coinsText;
+
+//    [Header("Healthpotions:")]
+//    public int healpotions;
+//    public int maxHealthpotionsSlots;
+//    public Text healpotionsText;
+//    public Text MaxHealthpotionsSlotsText;
+//    public bool isHealpotionCollectable;
+
+
+//    [Header("Player Health:")]
+//    public int currentHealth;
+//    public int maxHealth;
+//    public Text currentHealthText;
+//    public Text maxHealthText;
+
+//    // Cached References
+//    Hero hero;
+
+
+
+//    public void Start()
+//    {
+//        hero = FindObjectOfType<Hero>();      
+//    }
+
+//    public void Update() 
+//    {
+//        if (healpotions < maxHealthpotionsSlots) // begrenzen der einsammelbaren Heiltränke
+//        { 
+//           isHealpotionCollectable = true;
+//        }
+//        else if (healpotions == maxHealthpotionsSlots) 
+//        {
+//            isHealpotionCollectable = false;
+//        }
+//    }
+
+//    public void AddCoin(int getCoins)
+//    {
+//        coins += getCoins;
+//        UpdateCoinsText(coins);
+//    }
+//    public void UpdateCoinsText(int coins)
+//    {
+//        coinsText.text = "x" + coins.ToString();
+//    }
+
+//    //********************************** Healpotions
+//    public void AddHealpotions(int getHealpotions)
+//    {
+//        healpotions += getHealpotions;
+//        UpdateHealpotionsText(healpotions);
+//    }
+//    public void UpdateHealpotionsText(int healpotions)
+//    {
+//        healpotionsText.text = "x" + healpotions.ToString();
+//    }
+   
+//    public void UseHealpotions()
+//    {
+//        healpotions--; // benutzt - Wert um Healpotion zu nutzen
+//        UpdateHealpotionsText(healpotions);
+//    }
+
+//    //********************************** ShowMaxHealpotionsText
+//    public void GetMaxHealthpotionSlotsText(int getMaxHealpotions) // noch einbauen
+//    {
+//        maxHealthpotionsSlots += getMaxHealpotions;
+//        UpdateMaxHealthText(maxHealthpotionsSlots);
+//    }
+//    public void UpdateMaxHealpotionsSlots(int maxHealthpotions) // noch einbauen
+//    {
+//        MaxHealthpotionsSlotsText.text = "/" + maxHealthpotions.ToString();
+//    }
+
+//    //********************************** ShowCurrentHealthText
+//    public void GetCurrentHealthText(int getCurrentHealth) // noch einbauen
+//    {
+//        currentHealth += getCurrentHealth;
+//        UpdateCurrentHealthText(currentHealth);
+//    }
+//    public void UpdateCurrentHealthText(int currentHealth)
+//    {
+//        currentHealthText.text = currentHealth.ToString();
+//    }
+//    public void DecreaseCurrentHealthText(int getDamage) // noch einbauen
+//    {
+//        currentHealth -= getDamage; // benutzt - Wert um Healpotion zu nutzen
+//        UpdateHealpotionsText(currentHealth);
+//    }
+
+//    //********************************** ShowMaxHealthText
+//    public void GetMaxHealthText(int getMaxHealth) // noch einbauen
+//    {
+//        maxHealth += getMaxHealth;
+//        UpdateMaxHealthText(maxHealth);
+//    }
+//    public void UpdateMaxHealthText(int maxHealth) // noch einbauen
+//    {
+//        maxHealthText.text = "/" + maxHealth.ToString();
+//        Debug.Log("UpdateMaxHealthText wurde ausgeführt");
+//    }
+//    /*
+//    public void DecreaseMaxHealthText(int getMaxHealth)
+//    {
+//        maxHealth -= getMaxHealth; // benutzt - Wert um Healpotion zu nutzen
+//        UpdateMaxHealthText(getMaxHealth);
+//    }
+//    */
+//}
+
+
+///*
+//    public void ShowStageText(int amount)
+//    {
+//        // stageText.gameObject.SetActive(true);
+//        stageText.text = "Stage " + amount;
+
+//        // Invoke("DeactivateStagetext", 3f);
+//    }
+
+//    //  void DeactivateStagetext()
+//    // {
+//    //     stageText.gameObject.SetActive(false);
+//    //      CancelInvoke("DeactivateStagetext");
+//    //  }
+//*/
--- Assets/Scripts/Williams Scripts/Hero.cs
+++ Assets/Scripts/Williams Scripts/Hero.cs
@@ -7,13 +7,11 @@ public class Hero : MonoBehaviour
     GameManager gamemanager; // Verknüpfung mit Gamemanager
 
     [Header("Georges attributes:")]
-    public int maxHealth = 100;
-    public int currentHealth;
+    private int maxHealth = 100;
+    private int currentHealth;
+    private int maxHealpotionsSlots = 3; 
     public float moveSpeed = 5f;
-    public float detonationTimer = 0f;
-    public int healthRecover = 10;
     public int useHealpotions = 1;
-    public int maxHealpotionsSlots = 3; //******************************** Noch einbauen im UI und Player
 
     [Header("Arrows:")]
     public GameObject bulletPrefab;
@@ -46,7 +44,6 @@ public class Hero : MonoBehaviour
     {
         // Updaten der Stats im Gamemanager
         gamemanager = FindObjectOfType<GameManager>();
-        // drehen des Projektile des Charakters in Blickrichtung 
         rb = GetComponent<Rigidbody2D>();
         rb.velocity = transform.right * moveSpeed * 10;
         transform.LookAt(transform.position, this.rb.velocity);
@@ -54,13 +51,13 @@ public class Hero : MonoBehaviour
         currentHealth = maxHealth;          // verändet Wert der Healtbar
         healthBar.SetMaxHealth(maxHealth);
         // Update UI CurrentHealth, MaxHealth
-        gamemanager.UpdateMaxHealthText(maxHealth);
-        gamemanager.UpdateCurrentHealthText(currentHealth);
-        gamemanager.UpdateMaxHealpotionsSlots(maxHealpotionsSlots);
+        maxHealth= gamemanager.maxHp;
+        currentHealth = gamemanager.hp;
+        maxHealpotionsSlots = gamemanager.maxHealthpotions;
 
         state = State.Normal; // fürs Rollen
     }
-    
+
     // Update is called once per frame
     void Update()
     {
@@ -79,9 +76,9 @@ public class Hero : MonoBehaviour
                 */
 
                 // animator einfügen
-                 animator.SetFloat("Horizontal", movement.x);
-                 animator.SetFloat("Vertical", movement.y);
-                 animator.SetFloat("Speed", movement.sqrMagnitude);
+                animator.SetFloat("Horizontal", movement.x);
+                animator.SetFloat("Vertical", movement.y);
+                animator.SetFloat("Speed", movement.sqrMagnitude);
 
                 float shootHor = Input.GetAxis("ShootHorizontal");
                 // transform.forward = new Vector3(shootHor, 0, 0);
@@ -90,11 +87,6 @@ public class Hero : MonoBehaviour
                 {
                     Shoot(shootHor, shootVert);
                     lastFire = Time.time;
-                }
-
-                if (Input.GetKeyDown(KeyCode.Q) && gamemanager.healpotions > 0 && currentHealth != maxHealth) // Gamemanager fragen wie viele heiltränke wir haben, wemm über 1 = true, wenn Leben Voll = false
-                {
-                    HealDamage(healthRecover);
                 }
 
                 if (Input.GetKeyDown(KeyCode.Mouse1))
@@ -106,19 +98,19 @@ public class Hero : MonoBehaviour
                     // Play Roll Animation (rollDir)
                 }
                 break;
-                case State.Rolling:
+            case State.Rolling:
                 float rollSpeedDropMultiplier = 5f;
                 rollSpeed -= rollSpeed * rollSpeedDropMultiplier * Time.deltaTime;
 
                 float rollSpeedMinimum = 50f;
-                    if (rollSpeed < rollSpeedMinimum)
-                    {
-                        state = State.Normal;
-                    }
+                if (rollSpeed < rollSpeedMinimum)
+                {
+                    state = State.Normal;
+                }
                 break;
         }
     }
-    
+
 
     void FixedUpdate()
     {
@@ -131,7 +123,7 @@ public class Hero : MonoBehaviour
                 rb.velocity = rollDir * rollSpeed;
                 break;
         }
-        
+
     }
 
     public void TakeDamage(int damage)
@@ -139,7 +131,7 @@ public class Hero : MonoBehaviour
         currentHealth -= damage;
         healthBar.SetHealth(currentHealth);
         // Updaten des Healthbartextes im UI
-        gamemanager.UpdateCurrentHealthText(currentHealth);
+        gamemanager.hp = currentHealth;
         if (currentHealth <= 0)
         {
 
@@ -159,7 +151,7 @@ public class Hero : MonoBehaviour
 
     public void HealDamage(int healthRecover)
     {
-        currentHealth += healthRecover;
+        currentHealth += gamemanager.healthRecover;
 
         if (currentHealth > maxHealth) // falls man mit einen Heiltrank mehr heilen sollte als man hat wird der Wert auf den Max wert gesetzt
         {
@@ -167,22 +159,224 @@ public class Hero : MonoBehaviour
         }
 
         healthBar.SetHealth(currentHealth);
-        // Updaten des Healthbartextes im UI
-        gamemanager.UpdateCurrentHealthText(currentHealth);
-        // Updaten der HeiltrankAnzahl im UI
-        gamemanager.UseHealpotions();
-        // Updaten der HeiltrankSlotsAnzahl im UI
-        gamemanager.UpdateMaxHealpotionsSlots(maxHealpotionsSlots);
-
     }
 
-    void Shoot (float x, float y) // Berechnen der Schussgeschwindigkeit und Spawn
+    void Shoot(float x, float y) // Berechnen der Schussgeschwindigkeit und Spawn
     {
         GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity) as GameObject;
-       // bullet.AddComponent<Rigidbody2D>().gravityScale = 0; // fügt dem Schuss einen Rigidbody hinzu, und setzt gravity auf 0
+        // bullet.AddComponent<Rigidbody2D>().gravityScale = 0; // fügt dem Schuss einen Rigidbody hinzu, und setzt gravity auf 0
         bullet.GetComponent<Rigidbody2D>().velocity = new Vector3
         ((x < 0) ? Mathf.Floor(x) * bulletSpeed : Mathf.Ceil(x) * bulletSpeed, // if statement in einer zeile, prüfung ob x kleiner ist als 0, falls ja setzte den Wert auf - und führe die Rechnung aus
         (y < 0) ? Mathf.Floor(y) * bulletSpeed : Mathf.Ceil(y) * bulletSpeed,
         0);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class Hero : MonoBehaviour
+//{
+//    GameManager gamemanager; // Verknüpfung mit Gamemanager
+
+//    [Header("Georges attributes:")]
+//    public int maxHealth = 100;
+//    public int currentHealth;
+//    public float moveSpeed = 5f;
+//    public float detonationTimer = 0f;
+//    public int healthRecover = 10;
+//    public int useHealpotions = 1;
+//    public int maxHealpotionsSlots = 3; //******************************** Noch einbauen im UI und Player
+
+//    [Header("Arrows:")]
+//    public GameObject bulletPrefab;
+//    public float bulletSpeed = 5.0f;
+//    public float fireDelay = 0.5f;
+//    private float lastFire;
+
+//    [Header("References:")]
+//    //public Animator playerAnimator;
+//    private Rigidbody2D rb;
+//    public Animator animator;
+//    public HealthBar healthBar;
+//    public GameObject Explosion;
+
+//    [Header("Statistics:")]
+//    public int healpotionsInInventory;
+//    private Vector2 movement; // zwischenspeicherung von bewegungswerten
+
+//    private enum State // Statemachine fürs Rollen
+//    {
+//        Normal,
+//        Rolling,
+//    }
+
+//    private Vector3 rollDir; //********* new roll
+//    private State state;
+//    private float rollSpeed;
+
+//    void Start()
+//    {
+//        // Updaten der Stats im Gamemanager
+//        gamemanager = FindObjectOfType<GameManager>();
+//        // drehen des Projektile des Charakters in Blickrichtung 
+//        rb = GetComponent<Rigidbody2D>();
+//        rb.velocity = transform.right * moveSpeed * 10;
+//        transform.LookAt(transform.position, this.rb.velocity);
+
+//        currentHealth = maxHealth;          // verändet Wert der Healtbar
+//        healthBar.SetMaxHealth(maxHealth);
+//        // Update UI CurrentHealth, MaxHealth
+//        gamemanager.UpdateMaxHealthText(maxHealth);
+//        gamemanager.UpdateCurrentHealthText(currentHealth);
+//        gamemanager.UpdateMaxHealpotionsSlots(maxHealpotionsSlots);
+
+//        state = State.Normal; // fürs Rollen
+//    }
+    
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        switch (state)
+//        {
+//            case State.Normal:
+
+//                //Char Movement
+//                movement.x = Input.GetAxis("Horizontal");
+//                movement.y = Input.GetAxis("Vertical");
+//                /////////*********************************************** Test Rotation
+//                /*
+//                Vector3 newPosition = new Vector3(movement.x, 0.0f, movement.y);
+//                transform.LookAt(newPosition + transform.position);
+//                transform.Translate(newPosition * moveSpeed * Time.deltaTime, Space.World);
+//                */
+
+//                // animator einfügen
+//                 animator.SetFloat("Horizontal", movement.x);
+//                 animator.SetFloat("Vertical", movement.y);
+//                 animator.SetFloat("Speed", movement.sqrMagnitude);
+
+//                float shootHor = Input.GetAxis("ShootHorizontal");
+//                // transform.forward = new Vector3(shootHor, 0, 0);
+//                float shootVert = Input.GetAxis("ShootVertical");
+//                if ((shootHor != 0 || shootVert != 0) && Time.time > lastFire + fireDelay)
+//                {
+//                    Shoot(shootHor, shootVert);
+//                    lastFire = Time.time;
+//                }
+
+//                if (Input.GetKeyDown(KeyCode.Q) && gamemanager.healpotions > 0 && currentHealth != maxHealth) // Gamemanager fragen wie viele heiltränke wir haben, wemm über 1 = true, wenn Leben Voll = false
+//                {
+//                    HealDamage(healthRecover);
+//                }
+
+//                if (Input.GetKeyDown(KeyCode.Mouse1))
+//                {
+//                    //TakeDamage(20);
+//                    rollDir = movement;
+//                    rollSpeed = 55f;
+//                    state = State.Rolling;
+//                    // Play Roll Animation (rollDir)
+//                }
+//                break;
+//                case State.Rolling:
+//                float rollSpeedDropMultiplier = 5f;
+//                rollSpeed -= rollSpeed * rollSpeedDropMultiplier * Time.deltaTime;
+
+//                float rollSpeedMinimum = 50f;
+//                    if (rollSpeed < rollSpeedMinimum)
+//                    {
+//                        state = State.Normal;
+//                    }
+//                break;
+//        }
+//    }
+    
+
+//    void FixedUpdate()
+//    {
+//        switch (state)
+//        {
+//            case State.Normal:
+//                rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+//                break;
+//            case State.Rolling:
+//                rb.velocity = rollDir * rollSpeed;
+//                break;
+//        }
+        
+//    }
+
+//    public void TakeDamage(int damage)
+//    {
+//        currentHealth -= damage;
+//        healthBar.SetHealth(currentHealth);
+//        // Updaten des Healthbartextes im UI
+//        gamemanager.UpdateCurrentHealthText(currentHealth);
+//        if (currentHealth <= 0)
+//        {
+
+//            // Spiele Sound ab passiert in der explosion
+
+//            // Spiele Effect ab
+//            if (Explosion != null)
+//            {
+//                Instantiate(Explosion, transform.position, Quaternion.identity);
+//            }
+
+//            // Zerstöre Spieler, Teleportiere ihn zurück zur Stadt
+//            // Destroy(gameObject, detonationTimer);
+//            Debug.Log("Du bist gestorben");
+//        }
+//    }
+
+//    public void HealDamage(int healthRecover)
+//    {
+//        currentHealth += healthRecover;
+
+//        if (currentHealth > maxHealth) // falls man mit einen Heiltrank mehr heilen sollte als man hat wird der Wert auf den Max wert gesetzt
+//        {
+//            currentHealth = maxHealth;
+//        }
+
+//        healthBar.SetHealth(currentHealth);
+//        // Updaten des Healthbartextes im UI
+//        gamemanager.UpdateCurrentHealthText(currentHealth);
+//        // Updaten der HeiltrankAnzahl im UI
+//        gamemanager.UseHealpotions();
+//        // Updaten der HeiltrankSlotsAnzahl im UI
+//        gamemanager.UpdateMaxHealpotionsSlots(maxHealpotionsSlots);
+
+//    }
+
+//    void Shoot (float x, float y) // Berechnen der Schussgeschwindigkeit und Spawn
+//    {
+//        GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity) as GameObject;
+//       // bullet.AddComponent<Rigidbody2D>().gravityScale = 0; // fügt dem Schuss einen Rigidbody hinzu, und setzt gravity auf 0
+//        bullet.GetComponent<Rigidbody2D>().velocity = new Vector3
+//        ((x < 0) ? Mathf.Floor(x) * bulletSpeed : Mathf.Ceil(x) * bulletSpeed, // if statement in einer zeile, prüfung ob x kleiner ist als 0, falls ja setzte den Wert auf - und führe die Rechnung aus
+//        (y < 0) ? Mathf.Floor(y) * bulletSpeed : Mathf.Ceil(y) * bulletSpeed,
+//        0);
+//    }
+//}
