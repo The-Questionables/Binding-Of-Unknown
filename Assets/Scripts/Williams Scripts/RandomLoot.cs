411da4a3627dd3092791c47786b15689dd82b094
WilliamKieburg <battle-franky@hotmail.de>
2021-07-26 16:47:57 +0200

    Merge?

--- Assets/Scripts/Williams Scripts/RandomLoot.cs
+++ Assets/Scripts/Williams Scripts/RandomLoot.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class RandomLoot : MonoBehaviour
 {
-    [SerializeField]
-    [Tooltip("Amount Of Nothing That u want to Add to The List")]
-    private int ChanceForNothing;
     public GameObject[] LootList; // Array an Items
-    private int random; // speichert kurz zufälligen Wert aus dem Array
+    private int random; // speichert kurz zufï¿½lligen Wert aus dem Array
     private int random2;
-    private bool lootSpawned = false; // sorgt dafür das Räume nicht gleichzeitig spawnen
+    private bool lootSpawned = false; // sorgt dafï¿½r das Rï¿½ume nicht gleichzeitig spawnen
     public bool Drop_Multiple = false;
     [SerializeField]
     private uint amount;
+
+    void Start()
+    {
+        //Invoke("Spawn", 0.2f); // aktiviert Methode mit einen Time delay
+    }
 
     public void LootSpawn()
     {
@@ -22,7 +24,7 @@ public class RandomLoot : MonoBehaviour
             random2 = Random.Range(0, (ChanceForNothing + LootList.Length+1));
             if (random2 > ChanceForNothing)
             {
-                random = Random.Range(0, LootList.Length); // Sucht zufälligen Längenwert des BottomRoom Arrays aus
+                random = Random.Range(0, LootList.Length); // Sucht zufï¿½lligen Lï¿½ngenwert des BottomRoom Arrays aus
                 Instantiate(LootList[random], transform.position, Quaternion.identity); // Clonen eines Objektes und erstellen
             }
         }
@@ -60,8 +62,8 @@ using UnityEngine;
 public class RandomLoot : MonoBehaviour
 {
     public GameObject[] LootList; // Array an Items
-    private int random; // speichert kurz zufälligen Wert aus dem Array
-    private bool lootSpawned = false; // sorgt dafür das Räume nicht gleichzeitig spawnen
+    private int random; // speichert kurz zufï¿½lligen Wert aus dem Array
+    private bool lootSpawned = false; // sorgt dafï¿½r das Rï¿½ume nicht gleichzeitig spawnen
     public bool Drop_Multiple = false;
     [SerializeField]
     private uint amount;
@@ -76,7 +78,7 @@ public class RandomLoot : MonoBehaviour
 
         if (lootSpawned == false && Drop_Multiple == false)
         {
-            random = Random.Range(0, LootList.Length); // Sucht zufälligen Längenwert des BottomRoom Arrays aus
+            random = Random.Range(0, LootList.Length); // Sucht zufï¿½lligen Lï¿½ngenwert des BottomRoom Arrays aus
 
             // Instantiate(LootList[UnityEngine.Random.Range(0, 6)], transform.position, Quaternion.identity); // Clonen eines Objektes und erstellen
             // Instantiate(templates.bottomRooms[random], transform.position, Quaternion.identity); // Clonen eines Objektes und erstellen
