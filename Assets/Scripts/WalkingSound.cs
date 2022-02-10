3bf050e4cf29a469efbac3e4060e769fba5ce53e
KrakenTim <KrakenTim@freenet.de>
2022-02-10 01:55:59 +0100

    WalkSound Update

--- Assets/Scripts/WalkingSound.cs
+++ Assets/Scripts/WalkingSound.cs
@@ -6,13 +6,18 @@ public class WalkingSound : MonoBehaviour
 {
     public AudioSource Step1;
     public AudioSource Step2;
+    private float pitch;
 
     public void FirstStep()
     {
+        pitch = Random.Range(0.8f, 1.2f);
+        Step1.pitch = pitch;
         Step1.Play();
     }
     public void SecondStep()
     {
+        pitch = Random.Range(0.8f, 1.2f);
+        Step2.pitch = pitch;
         Step2.Play();
     }
 
